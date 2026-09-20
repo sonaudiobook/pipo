203a3d06989c546264ea25a013b66d6b36764b91
sonaudiobook <sonaudiobook@gmail.com>
2026-09-19 18:39:42 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const CACHE_NAME = 'pipo-cuento-v1';
+const assetsToCache = [
+  'index.html',
+  'javascript/config.js',
+  'javascript/LoadingJS.js',
+  'javascript/resource_skeleton.js',
+  'javascript/editor.js',
+  'javascript/BookPreview.js',
+  'files/search/book_config.js',
+  'files/extfile/icon-192.png',
+  'files/extfile/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(assetsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
